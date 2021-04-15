3d2d7fdf6eb970100017975701218127b41b8b85
laylalee0124 <jaha___@naver.com>
2021-04-15 12:36:29 +0900

    apr 15

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -18,6 +18,7 @@ public class ResponseJsonServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         //Content-Type: application/json
         response.setHeader("content-type", "application/json");
         response.setCharacterEncoding("utf-8");
