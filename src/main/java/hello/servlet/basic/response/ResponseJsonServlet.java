e45b2a41b7e5164af10386a31448e09122e8a66f
laylalee0124 <jaha___@naver.com>
2021-04-15 12:38:02 +0900

    apr 15

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-// [ HTTP 응답 데이터 - API JSON ]
+// [ HTTP 응답 데이터 - API JSON ] -
 @WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
 public class ResponseJsonServlet extends HttpServlet {
 
